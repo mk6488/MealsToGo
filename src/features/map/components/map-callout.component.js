d1fbd61ee05f3d8f7066085e38a0a3ce1bbb908f
mk6488 <mk3819@gmail.com>
2023-05-16 10:47:35 +0100

    android bug fix

--- src/features/map/components/map-callout.component.js
+++ src/features/map/components/map-callout.component.js
@@ -3,5 +3,5 @@ import React from "react";
 import { CompactRestaurantInfo } from "../../../components/restaurant/compact-restaurant.component";
 
 export const MapCallout = ({ restaurant }) => {
-  return <CompactRestaurantInfo restaurant={restaurant} />;
+  return <CompactRestaurantInfo isMap restaurant={restaurant} />;
 };
